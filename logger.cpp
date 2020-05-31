2ce247390e64b6f5e9b18bbdc94a59868272a64a
DaNicolescu <daniel.nicolescu10@gmail.com>
2020-05-31 19:05:27 +0300

    WIP parsing multiple writes modbus packets

--- logger.cpp
+++ logger.cpp
@@ -83,6 +83,36 @@ void get_modbus_single_write(struct modbus_single_write *modbus_struct,
     modbus_struct->value = htons(modbus_struct->value);
 }
 
+void get_modbus_multiple_write_query(
+    struct modbus_multiple_write_query *modbus_struct, const uint8_t *payload)
+{
+    memcpy(modbus_struct, payload, sizeof(struct modbus_tcp_generic) + 5);
+
+    modbus_struct->starting_address = htons(modbus_struct->starting_address);
+    modbus_struct->num_of_points = htons(modbus_struct->num_of_points);
+
+    modbus_struct->data = (uint8_t*) malloc(modbus_struct->byte_count);
+
+    if (!modbus_struct->data) {
+        std::cout << "Failed to allocate memory" << std::endl;
+
+        return;
+    }
+
+    memcpy(modbus_struct->data, payload + sizeof(struct modbus_tcp_generic) + 5,
+           modbus_struct->byte_count);
+}
+
+void get_modbus_multiple_write_response(
+    struct modbus_multiple_write_response *modbus_struct,
+    const uint8_t *payload)
+{
+    memcpy(modbus_struct, payload, sizeof(struct modbus_tcp_generic) + 4);
+
+    modbus_struct->starting_address = htons(modbus_struct->starting_address);
+    modbus_struct->num_of_points = htons(modbus_struct->num_of_points);
+}
+
 void my_packet_handler(uint8_t *args, const struct pcap_pkthdr *header,
                        const uint8_t *packet)
 {
--- logger.h
+++ logger.h
@@ -1,18 +1,18 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
-#define ETH_HDR_LEN		    0x0E
+#define ETH_HDR_LEN                 0x0E
 
-#define READ_COIL_STATUS	    0x01
-#define READ_INPUT_STATUS	    0x02
-#define READ_HOLDING_REGISTERS	    0x03
-#define READ_INPUT_REGISTERS	    0x04
-#define FORCE_SINGLE_COIL	    0x05
-#define PRESET_SINGLE_REGISTER	    0x06
-#define READ_EXCEPTION_STATUS	    0x07
-#define FORCE_MULTIPLE_COILS	    0x0F
+#define READ_COIL_STATUS            0x01
+#define READ_INPUT_STATUS           0x02
+#define READ_HOLDING_REGISTERS      0x03
+#define READ_INPUT_REGISTERS        0x04
+#define FORCE_SINGLE_COIL           0x05
+#define PRESET_SINGLE_REGISTER      0x06
+#define READ_EXCEPTION_STATUS       0x07
+#define FORCE_MULTIPLE_COILS        0x0F
 #define PRESET_MULTIPLE_REGISTERS   0x10
-#define REPORT_SLAVE_ID		    0x11
+#define REPORT_SLAVE_ID             0x11
 
 struct modbus_tcp_generic {
     uint16_t transaction_id;
@@ -46,6 +46,12 @@ struct modbus_multiple_write_query {
     uint16_t num_of_points;
     uint8_t byte_count;
     uint8_t *data;
+} __attribute__((packed));
+
+struct modbus_multiple_write_response {
+    struct modbus_tcp_generic generic_header;
+    uint16_t starting_address;
+    uint16_t num_of_points;
 } __attribute__((packed));
 
 struct modbus_tcp_all {
